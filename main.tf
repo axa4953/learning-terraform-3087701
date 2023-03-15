2a16ed4824a43e02f1aaa1ddb8fc89343a655314
axa4953 <34874748+axa4953@users.noreply.github.com>
2023-03-15 17:21:08 +1100

    c

--- main.tf
+++ main.tf
@@ -44,9 +44,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-
-  vpc_id = data.aws_vpc.default.id
+  
   name = "blog_new"
+  vpc_id = module.vpc.public_subnets[0]
+  
   ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
